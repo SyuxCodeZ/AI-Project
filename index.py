e52c83a8a7b7b0775a0bb9076b5fe091b0692b55
SyuxCodeZ <cuberdytm@gmail.com>
2025-10-29 21:35:25 +0600

    Add files via upload

--- index.py
+++ index.py
@@ -1,33 +1,42 @@
 import cv2
-import time
+import numpy as np
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
-    print("ERROR 404")
+    print("error")
     exit()
 
-time.sleep(1)
-
 while True:
-    ret,frame = cap.read()
-
+    ret, frame = cap.read()
     if not ret:
-        print("ERROR 404")
+        print("error")
         break
 
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30,30))
+    lower_skin = np.array([0, 20, 70], dtype = np.uint8)
+    upper_skin = np.array([20, 255, 255], dtype = np.uint8)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    mask = cv2.inRange(hsv, lower_skin, upper_skin)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.imshow("Face Detection - Press Q to Quit", frame)
+    if contours:
+        max_contour = max(contours, key = cv2.contourArea)
+        if cv2.contourArea(max_contour) > 500:
+            x, y, w, h = cv2.boundingRect(max_contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+            center_x = int(x + w / 2)
+            center_y = int(y + h / 2)
+
+            cv2.circle(frame, (center_x), (center_y), 5, (0, 0, 255), -1)
+
+        cv2.imshow('Original Frame', frame)
+        cv2.imshow('Filtered Frame', result)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 cap.release()
 cv2.destroyAllWindows()
