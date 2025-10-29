0e44f80f0d73db93f9c5aa915f03fcea491af8af
SyuxCodeZ <cuberdytm@gmail.com>
2025-10-29 21:43:08 +0600

    Add files via upload

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ while True:
 
     mask = cv2.inRange(hsv, lower_skin, upper_skin)
     result = cv2.bitwise_and(frame, frame, mask=mask)
-    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
         max_contour = max(contours, key = cv2.contourArea)
@@ -30,12 +30,12 @@ while True:
             center_x = int(x + w / 2)
             center_y = int(y + h / 2)
 
-            cv2.circle(frame, (center_x), (center_y), 5, (0, 0, 255), -1)
+            cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
 
-        cv2.imshow('Original Frame', frame)
-        cv2.imshow('Filtered Frame', result)
+    cv2.imshow('Original Frame', frame)
+    cv2.imshow('Filtered Frame', result)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 cap.release()
