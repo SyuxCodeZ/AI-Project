ffb07cab497aa4cae2ba09c7c58a36d7b7483707
SyuxCodeZ <cuberdytm@gmail.com>
2025-11-11 20:34:40 +0600

    Add files via upload

--- index.py
+++ index.py
@@ -1,42 +1,58 @@
 import cv2
 import numpy as np
+import pyautogui
 
 cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("error")
-    exit()
+pyautogui.FAILSAFE = False
+scroll_speed = 100
+
+print("Hand Gesture Scrolling Programming has Started, press 'q' to quit")
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("error")
+        print("ERROR 404")
         break
 
+    frame = cv2.flip(frame, 1)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_skin = np.array([0, 20, 70], dtype = np.uint8)
-    upper_skin = np.array([20, 255, 255], dtype = np.uint8)
+    lower_skin = np.array([0, 30, 60], dtype = np.uint8)
+
+    upper_skin = np.array([20, 150, 255], dtype = np.uint8)
 
     mask = cv2.inRange(hsv, lower_skin, upper_skin)
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    mask = cv2.GaussianBlur(mask, (5, 5), 0)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
-        max_contour = max(contours, key = cv2.contourArea)
-        if cv2.contourArea(max_contour) > 500:
-            x, y, w, h = cv2.boundingRect(max_contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        max_contour = max(contours, key=lambda x: cv2.contourArea(x))
+        if cv2.contourArea(max_contour) > 4000:
+            hull = cv2.convexHull(max_contour)
+            cv2.drawContours(frame, [hull], -1, (0, 255, 0), 2)
 
-            center_x = int(x + w / 2)
-            center_y = int(y + h / 2)
+            hull_area = cv2.contourArea(hull)
+            contour_Area = cv2.contourArea(max_contour)
 
-            cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
+            if contour_Area > 0:
+                ratio = hull_area / contour_Area
 
-    cv2.imshow('Original Frame', frame)
-    cv2.imshow('Filtered Frame', result)
+                if ratio > 1.7:
+                    pyautogui.scroll(scroll_speed)
+                    cv2.putText(frame, "Scroll Up", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
+                elif ratio < 1.3:
+                    pyautogui.scroll(-scroll_speed)
+                    cv2.putText(frame, "Scroll Down", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+                else:
+                    cv2.putText(frame, "Neutral", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+
+    cv2.imshow("Hand Gesture Scroll Controller", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        break
 
 cap.release()
 cv2.destroyAllWindows()
